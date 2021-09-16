b3da38ce8134d93209e43731988ee64376dfd13e
Jean Carlos <jean.silva0096@gmail.com>
2021-09-16 14:04:19 -0300

    Project created

---
+++ src/test/java/com/digitalinovation/beerstock/BeerStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalinovation.beerstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
